7642e2b0640987d5ad252d7c4e267607fd2bc469
g54 <giftisacc54@gmail.com>
2023-05-01 20:17:22 +0300

    update

--- app/src/main/java/com/example/intent/PhoneCallActivity.java
+++ app/src/main/java/com/example/intent/PhoneCallActivity.java
@@ -4,22 +4,50 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-public class PhoneCallActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class PhoneCallActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
+    public EditText etPhoneNumber;
+    public Button btnCall;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_call);
 
+        //Navigation
         BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+
+        //Call
+        etPhoneNumber = (EditText)findViewById(R.id.etPhoneNumber);
+        btnCall = (Button) findViewById(R.id.btnCall);
+
+        //Navigation
         navigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
         navigationView.setSelectedItemId(R.id.phoneCall);
+
+        //Call
+        btnCall.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnCall){
+            if (etPhoneNumber.getText().length()>2){
+                String phoneNumber = etPhoneNumber.getText().toString();
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:"+phoneNumber));
+                startActivity(callIntent);
+            }
+        }
     }
 
     @Override

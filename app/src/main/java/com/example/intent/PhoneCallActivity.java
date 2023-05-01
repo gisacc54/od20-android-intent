4402083ea51c881ec1cf71c5e5d1ab6c5d7db001
g54 <giftisacc54@gmail.com>
2023-05-01 19:00:12 +0300

    update

--- app/src/main/java/com/example/intent/PhoneCallActivity.java
+++ app/src/main/java/com/example/intent/PhoneCallActivity.java
@@ -10,15 +10,16 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class PhoneCallActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_phone_call);
+
         BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
         navigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
-        navigationView.setSelectedItemId(R.id.calculator);
+        navigationView.setSelectedItemId(R.id.phoneCall);
     }
 
     @Override
@@ -26,10 +27,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         switch(item.getItemId())
         {
             case R.id.calculator:
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                overridePendingTransition(0,0);
                 return true;
             case R.id.phoneCall:
-                startActivity(new Intent(getApplicationContext(),PhoneCallActivity.class));
-                overridePendingTransition(0,0);
                 return true;
             case R.id.sendSms:
                 startActivity(new Intent(getApplicationContext(),SendSMSActivity.class));
--- app/src/main/java/com/example/intent/SendSMSActivity.java
+++ app/src/main/java/com/example/intent/SendSMSActivity.java
@@ -10,15 +10,16 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class SendSMSActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_send_smsactivity);
+
         BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
         navigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
-        navigationView.setSelectedItemId(R.id.calculator);
+        navigationView.setSelectedItemId(R.id.sendSms);
     }
 
     @Override
@@ -26,14 +27,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         switch(item.getItemId())
         {
             case R.id.calculator:
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                overridePendingTransition(0,0);
                 return true;
             case R.id.phoneCall:
                 startActivity(new Intent(getApplicationContext(),PhoneCallActivity.class));
                 overridePendingTransition(0,0);
                 return true;
             case R.id.sendSms:
-                startActivity(new Intent(getApplicationContext(),SendSMSActivity.class));
-                overridePendingTransition(0,0);
                 return true;
         }
         return false;

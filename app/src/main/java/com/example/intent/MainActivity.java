6b694bc8339dcf9446917ade67f46ed7210ff169
g54 <giftisacc54@gmail.com>
2023-05-01 19:50:25 +0300

    update

--- app/src/main/java/com/example/intent/MainActivity.java
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -6,19 +6,91 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
+    public EditText etFirstNumber,etSecondNumber;
+    public TextView tvAnswer;
+    public Button btnPlus,btnMinus,btnTimes,btnDivide,btnClear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Navigation
         BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+
+        //Calculator
+        etFirstNumber = (EditText)findViewById(R.id.etFirstNumber);
+        etSecondNumber = (EditText)findViewById(R.id.etSecondNumber);
+        tvAnswer = (TextView)findViewById(R.id.tvAnswer);
+        btnPlus = (Button) findViewById(R.id.btnPlus);
+        btnMinus = (Button)findViewById(R.id.btnMinus);
+        btnTimes = (Button)findViewById(R.id.btnTimes);
+        btnDivide = (Button)findViewById(R.id.btnDivide);
+        btnClear = (Button)findViewById(R.id.btnClear);
+
+        //Navigation
         navigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
         navigationView.setSelectedItemId(R.id.calculator);
+
+        //Calculator
+        btnPlus.setOnClickListener(this);
+        btnClear.setOnClickListener(this);
+        btnDivide.setOnClickListener(this);
+        btnTimes.setOnClickListener(this);
+        btnMinus.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if((etFirstNumber.getText().length()>0 && etSecondNumber.getText().length()>0)){
+            double number1 = Double.parseDouble(etFirstNumber.getText().toString());
+            double number2 = Double.parseDouble(etSecondNumber.getText().toString());
+            this.calculator(view.getId(),number1,number2);
+        }else if (view.getId() ==  R.id.btnClear){
+            etFirstNumber.setText("");
+            etSecondNumber.setText("");
+            tvAnswer.setText("");
+        }
+
+    }
+
+    public void calculator(int id,double number1,double number2){
+        double answer = 0.0;
+        String answerString = "";
+        switch (id){
+            case R.id.btnPlus:
+                answer = number1+number2;
+                answerString = Double.toString(answer);
+                break;
+            case R.id.btnMinus:
+                answer = number1-number2;
+                answerString = Double.toString(answer);
+                break;
+            case R.id.btnTimes:
+                answer = number1*number2;
+                answerString = Double.toString(answer);
+                break;
+            case R.id.btnDivide:
+                answer = number1/number2;
+                answerString = Double.toString(answer);
+                break;
+            case R.id.btnClear:
+                etFirstNumber.setText("");
+                etSecondNumber.setText("");
+                break;
+        }
+        tvAnswer.setText(answerString);
     }
 
     @Override
